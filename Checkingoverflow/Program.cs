3dcbacd23db86804dcb4af8f3d6ea38ce14916b1
Muhammad Ali <muhammad.ali.ch@hotmail.com>
2017-02-26 23:58:37 -0600

    CheckingOverFlow
    C# Project

---
+++ Checkingoverflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Checkingoverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           short s = 900;
+            byte val = checked(byte)s;
+          
+            Console.WriteLine("{0}",s);
+            Console.ReadKey();
+
+
+
+        }
+         checked{
+               short sd = 900;
+            byte val1 = checked(byte)sd;
+               unchecked{}
+           
+           }
+    }
+}
